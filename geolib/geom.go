4617fc105737fa2e0d2acd737bbdab3872e67620
monkeyforecaster <p.rozas.larraondo@gmail.com>
2016-07-21 05:30:43 +0000

    Code clean a github issue solved

--- geolib/geom.go
+++ geolib/geom.go
@@ -75,21 +75,25 @@ func (p GDALPolygon) ReprojectToWGS84() GDALPolygon {
 
 	err := C.OGR_G_TransformTo(newPoly.Handler, desSRS)
         if err == 6 {
-		return newPoly
+                ringCoords := p.AsArray()
+		poly := PolygonFromCorners(ringCoords[0][0]-(ringCoords[0][0]*.01), 1, ringCoords[3][0]-(ringCoords[3][0]*.01), -1, p.ProjWKT())
+ 		polyWGS84 := poly.ReprojectToWGS84()
+                ringWGS84Coords := polyWGS84.AsArray()
+                newPoly = PolygonFromCorners(ringWGS84Coords[0][0], 90, ringWGS84Coords[3][0], -90, polyWGS84.ProjWKT())
 	}
 
 	return newPoly
 }
 
-func (p GDALPolygon) AsPolygon() string {
+func (p GDALPolygon) AsArray() [][]float64 {
 	var poly geo.Polygon
 	err := poly.UnmarshalWKT(p.ToWKT())
 
 	if err != nil {
 		fmt.Println(err)
 	}
-	//return poly
-	return p.ToWKT()
+        
+	return poly.AsArray()[0]
 }
 
 func GetPolygon(projWKT string, geoTrans []float64, xSize, ySize int) GDALPolygon {
@@ -118,3 +122,27 @@ func GetPolygon(projWKT string, geoTrans []float64, xSize, ySize int) GDALPolygo
 
 	return p
 }
+
+func PolygonFromCorners(ulX, ulY, lrX, lrY float64, projWKT string) GDALPolygon {
+
+	polyWKT := fmt.Sprintf("POLYGON ((%f %f,%f %f,%f %f,%f %f,%f %f))", ulX, ulY, ulX, lrY, lrX, lrY, lrX, ulY, ulX, ulY)
+
+        ppszData := C.CString(polyWKT)
+	defer C.free(unsafe.Pointer(ppszData))
+
+	hSRS := C.OSRNewSpatialReference(nil)
+	cProjWKT := C.CString(projWKT)
+	defer C.free(unsafe.Pointer(cProjWKT))
+
+	C.OSRImportFromWkt(hSRS, &cProjWKT)
+	
+	var hPt C.OGRGeometryH
+
+	_ = C.OGR_G_CreateFromWkt(&ppszData, hSRS, &hPt)
+
+	C.OGR_G_AssignSpatialReference(hPt, hSRS)
+	p := GDALPolygon{hPt}
+
+	return p
+}
+
--- geoparse/geoparser.go
+++ geoparse/geoparser.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"../geolib"
+	//geo "bitbucket.org/monkeyforecaster/geometry"
 	"bufio"
 	"encoding/json"
 	"fmt"
@@ -16,34 +17,35 @@ import (
 
 type GeoMetaData struct {
 	DataSetName    string            `json:"ds_name"`
-	TimeStamps     []string          `json:"timestamps"`
+	TimeStamps     []time.Time       `json:"timestamps"`
 	FileNameFields map[string]string `json:"filename_fields"`
-	Polygon        json.RawMessage   `json:"polygon"`
+	Polygon        string            `json:"polygon"`
+	PolygonWGS84   string            `json:"polygon_wgs84"`
 	RasterCount    int               `json:"raster_count"`
 	Type           string            `json:"array_type"`
 	XSize          int               `json:"x_size"`
 	YSize          int               `json:"y_size"`
 	ProjWKT        string            `json:"proj_wkt"`
+	Proj4          string            `json:"proj4"`
 	GeoTransform   []float64         `json:"geotransform"`
 }
 
 type GeoFile struct {
+	FileName *string       `json:"filename,omitempty"`
 	Driver   string        `json:"file_type"`
 	DataSets []GeoMetaData `json:"geo_metadata"`
 }
 
-//MCD12Q1.A2001001.h00v10.051.2014287161527.hdf
-//MCD43A4.A2001337.h29v11.005.2007082233323.hdf
-//MCD43A2.A2003025.h13v04.005.2007275170736.hdf
-
 var parserStrings map[string]string = map[string]string{"landsat": `LC(?P<mission>\d)(?P<path>\d\d\d)(?P<row>\d\d\d)(?P<year>\d\d\d\d)(?P<julian_day>\d\d\d)(?P<processing_level>[a-zA-Z0-9]+)_(?P<band>[a-zA-Z0-9]+)`,
 	"modis1":        `M(?P<satellite>[OD|YD])(?P<product>[0-9]+_[A-Z0-9]+).A[0-9]+.[0-9]+.(?P<collection_version>\d\d\d).(?P<year>\d\d\d\d)(?P<julian_day>\d\d\d)(?P<hour>\d\d)(?P<minute>\d\d)(?P<second>\d\d)`,
 	"modis2":        `^(?P<product>MCD\d\d[A-Z]\d).A[0-9]+.(?P<horizontal>h\d\d)(?P<vertical>v\d\d).(?P<resolution>\d\d\d).(?P<year>\d\d\d\d)(?P<julian_day>\d\d\d)(?P<hour>\d\d)(?P<minute>\d\d)(?P<second>\d\d)`,
-	"agdc_landsat1": `LS(?P<mission>\d)_(?P<sensor>[A-Z]+)_(?P<correction>[A-Z]+)_(?P<epsg>\d+)_(?P<x_coord>-?\d+)_(?P<y_coord>-?\d+)_(?P<year>\d\d\d\d).`}
+	"modisJP":       `^(?P<product>FC).v302.(?P<root_product>MCD\d\d[A-Z]\d).A(?P<year>\d\d\d\d)(?P<julian_day>\d\d\d).h(?P<horizontal>\d\d)v(?P<vertical>\d\d).(?P<resolution>\d\d\d).`,
+	"himawari8":     `^(?P<year>\d\d\d\d)(?P<month>\d\d)(?P<day>\d\d)(?P<hour>\d\d)(?P<minute>\d\d)(?P<second>\d\d)-P1S-(?P<product>ABOM[0-9A-Z_]+)-PRJ_GEOS141_(?P<resolution>\d+)-HIMAWARI8-AHI`,
+	"agdc_landsat1": `LS(?P<mission>\d)_(?P<sensor>[A-Z]+)_(?P<correction>[A-Z]+)_(?P<epsg>\d+)_(?P<x_coord>-?\d+)_(?P<y_coord>-?\d+)_(?P<year>\d\d\d\d).`,
+	"agdc_landsat2": `LS(?P<mission>\d)_OLI_(?P<sensor>[A-Z]+)_(?P<product>[A-Z]+)_(?P<epsg>\d+)_(?P<x_coord>-?\d+)_(?P<y_coord>-?\d+)_(?P<year>\d\d\d\d).`,
+	"agdc_dem":      `SRTM_(?P<product>[A-Z]+)_(?P<x_coord>-?\d+)_(?P<y_coord>-?\d+)_(?P<year>\d\d\d\d)(?P<month>\d\d)(?P<day>\d\d)(?P<hour>\d\d)(?P<minute>\d\d)(?P<second>\d\d)`}
 
 var parsers map[string]*regexp.Regexp = map[string]*regexp.Regexp{}
-
-//var timeExtractors map[string]func(map[string] string) time.Time = map[string]func(map[string] string) time.Time{"landsat":landsatTime, "modis1": modisTime, "modis2": modisTime}
 
 func init() {
 	for key, value := range parserStrings {
@@ -76,10 +78,17 @@ func parseName(filePath string) (map[string]string, time.Time) {
 func parseTime(nameFields map[string]string) time.Time {
 	if _, ok := nameFields["year"]; ok {
 		year, _ := strconv.Atoi(nameFields["year"])
-		t := time.Date(year, 0, 0, 0, 0, 0, 0, time.UTC)
+		t := time.Date(year, 1, 1, 0, 0, 0, 0, time.UTC)
 		if _, ok := nameFields["julian_day"]; ok {
 			julianDay, _ := strconv.Atoi(nameFields["julian_day"])
-			t = t.Add(time.Hour * 24 * time.Duration(julianDay))
+			t = t.Add(time.Hour * 24 * time.Duration(julianDay-1))
+		}
+		if _, ok := nameFields["month"]; ok {
+			if _, ok := nameFields["day"]; ok {
+				month, _ := strconv.Atoi(nameFields["month"])
+				day, _ := strconv.Atoi(nameFields["day"])
+				t = time.Date(year, time.Month(month), day, 0, 0, 0, 0, time.UTC)
+			}
 		}
 		if _, ok := nameFields["hour"]; ok {
 			hour, _ := strconv.Atoi(nameFields["hour"])
@@ -100,6 +109,7 @@ func parseTime(nameFields map[string]string) time.Time {
 }
 
 func main() {
+
 	s := bufio.NewScanner(os.Stdin)
 	for s.Scan() {
 		parts := strings.Split(s.Text(), "\t")
@@ -125,23 +135,29 @@ func main() {
 					poly := geolib.GetPolygon(ds.ProjWKT, ds.GeoTransform, ds.XSize, ds.YSize)
 					polyWGS84 := poly.ReprojectToWGS84()
 
-					var times []string
+					var times []time.Time
 					if nc_times, ok := ds.Extras["nc_times"]; ok {
-						times = nc_times
+						for _, timestr := range nc_times {
+							t, err := time.Parse("2006-01-02T15:04:05Z", timestr)
+							if err != nil {
+								fmt.Println(err)
+							}
+							times = append(times, t)
+						}
 					} else {
-						times = []string{timeStamp.Format("2006-01-02T15:04:05Z")}
+						times = []time.Time{timeStamp}
 					}
 
-					geoFile.DataSets = append(geoFile.DataSets, GeoMetaData{DataSetName: ds.DataSetName, TimeStamps: times, FileNameFields: nameFields, Polygon: json.RawMessage(polyWGS84.ToGeoJSON()), RasterCount: ds.RasterCount, Type: ds.Type, XSize: ds.XSize, YSize: ds.YSize, ProjWKT: ds.ProjWKT, GeoTransform: ds.GeoTransform})
-
+					geoFile.DataSets = append(geoFile.DataSets, GeoMetaData{DataSetName: ds.DataSetName, TimeStamps: times, FileNameFields: nameFields, Polygon: poly.ToWKT(), PolygonWGS84: polyWGS84.ToWKT(), RasterCount: ds.RasterCount, Type: ds.Type, XSize: ds.XSize, YSize: ds.YSize, ProjWKT: ds.ProjWKT, Proj4: poly.Proj4(), GeoTransform: ds.GeoTransform})
 				}
 			}
+
 			out, err := json.Marshal(&geoFile)
 			if err != nil {
 				fmt.Println(err)
 				os.Exit(1)
 			}
-			fmt.Printf("%s\tgdal\t%s\n", parts[0], string(out))
+			fmt.Printf("%s\ttsv\t%s\n", parts[0], string(out))
 		} else {
 			log.Printf("%s non parseable", parts[0])
 		}
