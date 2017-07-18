0de2fbcfdb0c0284726a18abc6e2c7543363cb00
monkeyforecaster <p.rozas.larraondo@gmail.com>
2017-07-18 16:32:09 +1000

    regexp fixed

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	proc "./processor"
 	"context"
 	"fmt"
+	"regexp"
 )
 
 func main() {
@@ -11,8 +12,9 @@ func main() {
 	ctx, ctxCancel := context.WithCancel(ctx)
 	errChan := make(chan error)
 	tp := proc.InitInfoPipeline(ctx, "localhost:6060", errChan)
+	contains := regexp.MustCompile(".*")
 	select {
-	case <-tp.Process("/g/data2/u39/public/data/modis/lpdaac-tiles-c5/MCD12Q1.051/2013.01.01/", ".*"):
+	case <-tp.Process("/g/data2/u39/public/data/modis/lpdaac-tiles-c5/MCD12Q1.051/2013.01.01/", contains):
 		fmt.Println("Pipeline Total Time")
 	case err := <-errChan:
 		fmt.Println(err)
